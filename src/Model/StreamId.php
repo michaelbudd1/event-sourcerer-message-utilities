69daa20dfdb9ba514c0091664b8535957d4bdc18
michaelbudd <michaelbudd6@gmail.com>
2025-02-19 09:02:19 +0000

    adds isAllStream method

--- src/Model/StreamId.php
+++ src/Model/StreamId.php
@@ -7,4 +7,11 @@ namespace PearTreeWebLtd\EventSourcererMessageUtilities\Model;
 final readonly class StreamId implements IsString
 {
     use FulfilIsString;
+
+    private const string ALL_STREAM = '*';
+
+    public function isAllStream(): bool
+    {
+        return self::ALL_STREAM === $this->value;
+    }
 }
