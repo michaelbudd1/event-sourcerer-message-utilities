44e4050cb682068aa3b70b76ef55477da3f85e36
michaelbudd <michaelbudd6@gmail.com>
2025-03-08 07:55:32 +0000

    adds markup to find rejected event inside payload

--- src/Model/MessageMarkup.php
+++ src/Model/MessageMarkup.php
@@ -6,5 +6,7 @@ namespace PearTreeWebLtd\EventSourcererMessageUtilities\Model;
 
 enum MessageMarkup: String
 {
-    case NewEventParser = '--- SYSTEM NEW EVENT ---';
+    case NewEventParser     = '--- SYSTEM NEW EVENT ---';
+    case RejectedEventStart = '--- REJECTED EVENT START ---';
+    case RejectedEventEnd   = '--- REJECTED EVENT END ---';
 }
