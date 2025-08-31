f3855c677ff2d96c2e14dd2dd424f6b3ff0e8168
michaelbudd <michaelbudd6@gmail.com>
2025-08-31 07:16:36 +0100

    wip

---
+++ src/Model/Event.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PearTreeWebLtd\EventSourcererMessageUtilities\Model;
+
+final readonly class Event
+{
+    public function __construct(
+        public Checkpoint $allSequenceCheckpoint,
+        public EventVersion $eventVersion,
+        public EventName $eventName,
+        public array $payload,
+        public StreamId $streamId,
+        public \DateTimeImmutable $eventOccurredAt,
+        public StreamId $catchupRequestStreamId
+    ) {}
+}
